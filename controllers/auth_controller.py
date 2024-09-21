2696aa9240869901c62df72a4bf6d8a31234d1d1
Venita Kissell <venitak@hotmail.com>
2024-09-21 18:00:16 +1000

    added more to auth controllers, cli controllers and requirements.txt

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -1,10 +1,13 @@
 from flask import Blueprint, request
-from models.user import User, user_schema
+from models.user import User, user_schema, UserSchema
 from init import bcrypt, db
+from datetime import timedelta
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from utils import auth_as_admin_decorator
 
-auth_bp = Blueprint("auth", __name__, url_prefix="/auth")
+auth_blue = Blueprint("auth", __name__, url_prefix="/auth")
 
-@auth_bp.route("/register", methods=["POST"])
+@auth_blue.route("/register", methods=["POST"])
 def register_user():
 
 #get the user data from the body, check if user exists
@@ -15,7 +18,7 @@ def register_user():
         email = body_data.get("email"),
         display_name = body_data.get("display_name")
     )
-#has protect the user password
+#hash protect the user password
     password = body_data.get("password")
     if password:
         user.password = bcrypt.generate_password_hash(password).decode("utf-8")
@@ -25,7 +28,68 @@ def register_user():
 #return confirmation
     return user_schema.dump(user), 201
 
+#search for existing user in the database and match it to the criteria
+@auth_blue.route("/login", methods=["POST"])
+def login_user():
+    # Get the user data from the body of the request
+    body_data = request.get_json()
+    # Find the user in DB with the matching email address
+    stmt = db.select(User).filter_by(email=body_data.get("email"))
+    user = db.session.scalar(stmt)
+    # If user exists in the database and password matches
+    if user and bcrypt.check_password_hash(user.password, body_data.get("password")):
+        # create JWT token for access
+        token = create_access_token(identity=str(user.id), expires_delta=timedelta(minutes=15))
+        # Reply to user 
+        return {"email": user.email, "is_admin": user.is_admin, "token": token}
+    # Else
+    else:
+        # Respond back with an error message
+        return {"error": "Invalid email or password, email or password is not in the database."}, 400
 
-
-
-
+#Find the user in the database and update with user changes
+# /auth/users/user_id
+@auth_blue.route("/users", methods=["PUT", "PATCH"])
+@jwt_required()
+def update_user():
+    # get the user fields from the body of the request
+    body_data = UserSchema().load(request.get_json(), partial=True)
+    password = body_data.get("password")
+    # get the user from the db
+    # SELECT * FROM user WHERE id= get_jwt_identity()
+    stmt = db.select(User).filter_by(id=get_jwt_identity())
+    user = db.session.scalar(stmt)
+    # if the user exists:
+    if user:
+        # then update the updated fields as required
+        user.name = body_data.get("name") or user.name
+        if password:
+            user.password = bcrypt.generate_password_hash(password).decode("utf-8")
+        # commit to the DB
+        db.session.commit()
+        # return a response
+        return user_schema.dump(user)
+    # else:
+    else:
+        # return an error response
+        return {"error": "The User does not exist."},
+    
+# Authorising Admin to delete a user
+@auth_blue.route("/users/<int:user_id>", methods=["DELETE"])
+@jwt_required()
+@auth_as_admin_decorator
+def delete_user(user_id):
+    # find the user with the id from the db
+    # SELECT * FROM users WHERE id==user_id;
+    stmt = db.select(User).filter_by(id=user_id)
+    user = db.session.scalar(stmt)
+    # if exists:
+    if user:
+        # delete the user
+        db.session.delete(user)
+        db.session.commit()
+        # return an acknowledgement message
+        return {"message": f"User with id {user_id} is deleted."}
+    else:
+        # return error message
+        return {"message": f"User with id {user_id} could not be found."}, 404
--- controllers/cli_controllers.py
+++ controllers/cli_controllers.py
@@ -1,6 +1,10 @@
+from datetime import date
+
 from flask import Blueprint
 from init import db, bcrypt
 from models.user import User
+from models.product import Product
+from models.review import Review
 
 
 #uses blueprints and calls db from the user.py
@@ -10,29 +14,117 @@ db_commands = Blueprint("db", __name__)
 @db_commands.cli.command("create")
 def create_tables():
     db.create_all()
-    print("Tables have been created")
+    print("Your Tables have been created")
 
-#create the list of user instances
+#Seed the tables with inserted data
 @db_commands.cli.command("seed")
 def seed_tables():
     users = [
        User(
-           email = "admin@email.com",
-           password = bcrypt.generate_password_hash('123456').decode("utf-8"),
+           name = "Venita Kissell",
+           email = "admin@gmail.com",
+           password = bcrypt.generate_password_hash('abc123').decode("utf-8"),
            admin_user= True
 
        ), 
        User(
-           name = "User 1",
-           email = "user1@email.com",
-           password = bcrypt.generate_password_hash("123456").decode("utf-8"),
-           display_name = "user_display1"
-       )
+           name = "Mary Brown",
+           email = "maryb@gmail.com",
+           password = bcrypt.generate_password_hash("abc123").decode("utf-8"),
+           display_name = "MaryB"
+       ),
+       User(
+           name = "John Jones",
+           email = "johnjones@gmail.com",
+           password = bcrypt.generate_password_hash("abc123").decode("utf-8"),
+           display_name = "JJJones"
+       ), 
+        User(
+           name = "Suki Nakagawa",
+           email = "nakagawas@gmail.com",
+           password = bcrypt.generate_password_hash("abc123").decode("utf-8"),
+           display_name = "Suki5"
+       ),
+       User(
+           name = "Sarah Banan",
+           email = "banwas@gmail.com",
+           password = bcrypt.generate_password_hash("abc123").decode("utf-8"),
+           display_name = "BanwaSS"
+       ),  
     ]
 
     db.session.add_all(users)
+
+    products = [
+        Product(
+            name = "Bike",
+            description = "Mountain Bike",
+            category = "Sport",
+            user = users[0]
+        ), 
+        Product(
+            title = "Freedom Lounge",
+            description = "3 seater L shaped lounge, grey",
+            category = "Furniture",
+            user = users[0]            
+        ), 
+        Product(
+            title = "Ikea Lamp",
+            description = "Ikea desk lamp",
+            category = "Electrical",
+            user = users[0]
+        ),
+        Product(
+            title = "MaxFactor Foundation",
+            description = "Dewy and Smooth 24 hour foundation",
+            category = "Beauty",
+            user = users[0]
+        ),
+        Product(
+            title = "Dr Beats HeadPhones",
+            description = "Dr Beats over ear HeadPhones",
+            category = "Technology",
+            user = users[0]
+        )
+        ]
+    db.session.add_all(reviews)
+
+    reviews = [
+        Review(
+            date = date.today(),
+            user = users[1],
+            product = products[3],
+            rating = "3",
+            comment = "Didn't really like the texture on my skin"
+        ),
+        Review(
+            date = date.today(),
+            user = users[3],
+            product = products[1],
+            rating = "4",
+            comment = "I really like this lamp, the quality is good for the price."
+        ),
+        Review(
+            date = date.today(),
+            user = users[4],
+            product = products[1],
+            rating = "5",
+            comment = "I really like this mountain bike, quality is good, price is good, its comfortable and well constructed"
+        )
+    ]
+
+    db.session.add_all(reviews)
+    
+
+
+
+
+
+
+
+
     db.session.commit()
-    print("Tables have been seeded successfully.")
+    print("Tables values have been seeded successfully.")
 
 @db_commands.cli.command("drop")
 def drop_tables():
